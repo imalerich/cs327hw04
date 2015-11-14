a88153ed83a307c6f83fbac73869cb06b110418f
Ian Malerich <ian.malerich@gmail.com>
2015-11-14 20:17:34 +0000

    created a base Channel class, mostly tested

---
+++ lib/Channel.cpp
@@ -0,0 +1,112 @@
+#include <exception>
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+#include "Channel.h"
+
+static const auto overflow_msg = "Sample exceeds this Channels bit resolution!";
+static const auto length_msg = "strict_data enabled: Channels must have the same number of samples!";
+static const auto invalid_msg = "strict_data enabled: Channels must have the same bit_res";
+
+Channel::Channel(unsigned BitRes, bool Strict) : bit_res{BitRes}, strict_data{Strict} {
+}
+
+Channel::Channel(const Channel &other) : bit_res{other.bit_res}, strict_data{other.strict_data} {
+	samples = other.samples;
+}
+
+Channel::Channel(const Channel &&other) : bit_res{other.bit_res}, strict_data{other.strict_data} {
+	samples = std::move(other.samples);
+}
+
+Channel& Channel::operator=(const Channel &other) {
+	samples = other.samples;
+	return *this;
+}
+
+Channel& Channel::operator=(const Channel &&other) {
+	samples = std::move(other.samples);
+	return *this;
+}
+
+ostream& operator<<(ostream &os, const Channel &channel) {
+	for (auto i = 0; i < (int)channel.samples.size(); i++) {
+		if (i != (int)channel.samples.size() -1 ) {
+			os << channel.samples[i] << ", ";
+		} else {
+			os << channel.samples[i];
+		}
+	}
+
+	return os;
+}
+
+Channel Channel::operator+(const Channel &other) {
+	// first check if we should allow Channel addition
+	if (other.strict_data || strict_data) {
+		if (other.samples.size() != samples.size()) {
+			throw length_error(length_msg);
+		} else if (other.bit_res != bit_res) {
+			throw invalid_argument(invalid_msg);
+		}
+	}
+
+	// all is good, perform the addition
+	Channel last = Channel(max(other.bit_res, bit_res), other.strict_data && strict_data);
+	for (auto i = 0; i < (int)max(other.samples.size(), samples.size()); i++) {
+		auto sample0 = i < (int)other.samples.size() ? other.samples[i] : 0;
+		auto sample1 = i < (int)samples.size() ? samples[i] : 0;
+
+		try {
+			last.push_sample(sample0 + sample1);
+		} catch (overflow_error e) {
+			// forward any overflow_error's that occur
+			throw;
+		}
+	}
+
+	return last;
+}
+
+Channel Channel::operator*(const double &scalar) {
+	Channel other = Channel(*this);
+	for (auto &sample : other.samples) {
+		if (is_valid_sample(sample * scalar)) {
+			sample *= scalar;
+		} else {
+			throw overflow_error(overflow_msg);
+		}
+	}
+
+	return other;
+}
+
+Channel Channel::operator-() {
+	Channel other = *this;
+	for (auto &sample : other.samples) {
+		if (is_valid_sample(-sample)) {
+			sample = -sample;
+		} else {
+			throw overflow_error(overflow_msg);
+		}
+	}
+
+	return other;
+}
+
+void Channel::push_sample(int sample) {
+	// check bounds specified by the bit resolution
+	if (!is_valid_sample(sample)) {
+		throw overflow_error(overflow_msg);
+	}
+
+	// all is good, add the sample to our samples vector
+	samples.push_back(sample);
+}
+
+bool Channel::is_valid_sample(int sample) {
+	static const int max_val = pow(2, bit_res) / 2;
+	static const int min_val = -(pow(2, bit_res) / 2 - 1);
+
+	return sample <= max_val && sample >= min_val;
+}
---
+++ lib/Channel.h
@@ -0,0 +1,94 @@
+#ifndef CHANNEL_H
+#define CHANNEL_H
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Representation of a channel. A channel requires that
+ * a bit res be specified it, it will then insure that all
+ * data added to it fits within the bitres.
+ * Internally all data is stored within a vector.
+ * A channel has no knowledge of it's sample rate, and
+ * therefore could not be 'played' back to the user at all.
+ */
+class Channel {
+public:
+	/**
+	 * \param BitRes Resolution (in bits) of the data stored in this channel.
+	 * \param Strict Determines whether or not to enforce strict rule swhen modifying multiple channels.
+	 */
+	Channel(unsigned BitRes, bool Strict = true);
+	Channel(const Channel &other);
+	Channel(const Channel &&other);
+	Channel& operator=(const Channel &&other);
+	Channel& operator=(const Channel &other);
+	friend ostream& operator<<(ostream &os, const Channel &channel);
+
+	/**
+	 * Scales each sample of this channel by the input scalar value.
+	 * Resulting integers are truncated.
+	 * If the scalar were to cause a sample to exceed this channels
+	 * bit resolution, this operator will thrown an overflow_exception.
+	 * \param scalar Scalar value to apply to each sample of this channel.
+	 */
+	Channel operator*(const double &scalar);
+
+	/**
+	 * Inverts each sample of this Channel.
+	 * Because the max_value of the bit_res is 1 greater than the 
+	 * absolute value of the min_value of the bit_res, this operator
+	 * will throw an overflow_exception when trying to invert
+	 * any instances of max_value.
+	 */
+	Channel operator-();
+
+	/**
+	 * Creates a new channel where each sample is the sum of this channels samples
+	 * and the samples of the 'other' Channel. 
+	 * If the sum of any sample exceeds the resulting bit resolution an overflow_error is thrown.
+	 * If either channel has 'strict_data' enabled the following requirements must be met:
+	 * 	Both channels must have the same bit_resolution (invalid_argument exception).
+	 * 	Both channels must have the same size (length_error exception).
+	 * Otherwise:
+	 * 	The higher 'bit_res' will be used.
+	 * 	The larger size will be used.
+	 * 	The smaller channel will be treated as if it had 0's beyond its end.
+	 * The resulting channel will have 'strict_data' disabled ONLY if
+	 * both input Channels have 'strict_data' disabled.
+	 */
+	Channel operator+(const Channel &other);
+
+	/**
+	 * Attempts to push the input sample to the end of this Channels sample vector.
+	 * If the sample data will not fit in this Channel's bit resolution, this 
+	 * method will throw an overflow_error exception.
+	 * is_valid_sample will be used to determine whether or not the sample is valid.
+	 * \param Input sample to append to the samples vector.
+	 */
+	void push_sample(int sample);
+
+	/**
+	 * Determines whether or not the input sample fits within
+	 * this Channel's bit resolution or not.
+	 * \param sample A sample to be tested.
+	 * \return Whether or not the sample is valid for this Channel.
+	 */
+	bool is_valid_sample(int sample);
+
+	/**
+	 * \returns The number of samples stored in this channel.
+	 */
+	inline size_t size() const {
+		return samples.size();
+	}
+	
+private:
+	vector<int> samples; /**< The array of samples representing this channel. */
+	const unsigned bit_res; /**< Resolution (in bits) of the data for this channel. */
+	const bool strict_data; /**< Determines whether to enforce equal channel size when adding. */
+};
+
+#endif
